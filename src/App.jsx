f77ff4075b00496df80c7e98061c8d701fd97b1c
helloYash112 <rajputyashwardhan17747@gmail.com>
2024-10-23 09:32:39 +0530

    uploding code to server

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,56 @@
 import Header from "./util/Header";
-import { TextField } from "./TextField";
+import { useState } from "react"
+import { InputFeild } from "./InputFeild";
+import {calculateInvestmentResults} from './util/investment';
+import { formatter } from "./util/investment";
+
 function App() {
+  const clientInput={
+    'initialInvestment':0,
+    'annualInvestment':0,
+    'expectedReturn':0,
+    'duration':0
+  
+  }
+  
+  
+  
+ const[clientData,udtClientData]=useState(clientInput);
+ const[res,sendRes]=useState(null);
+function reciveData(data,symbol){
+  udtClientData(oldData=>{
+    return{
+      ...oldData,
+      [symbol]:data
+    }
+  })
+}
+function calsy(datas){
+sendRes(calculateInvestmentResults(datas));
+}
   return(<div className="centre">
    <Header></Header>
    
    <ol className="input-group">
-   <TextField ></TextField>
-   <TextField></TextField>
-   <TextField></TextField>
-   <TextField></TextField>
-   </ol>
+  
+   <InputFeild udtInputData={reciveData} symbol='initialInvestment' ></InputFeild>
+   <InputFeild udtInputData={reciveData} symbol='annualInvestment'></InputFeild>
+   <InputFeild udtInputData={reciveData} symbol='expectedReturn'></InputFeild>
+   <InputFeild udtInputData={reciveData} symbol='duration'></InputFeild>
    
+   <button onClick={()=>calsy(clientData)}>calculate</button>
+   </ol>
+   {
+  res && <ul>
+    {
+      res.map((data,row)=>{
+        return<li key={row}>
+          {data['year']}  {formatter.format(data['interest'])} {formatter.format(data['valueEndOfYear'])}  {formatter.format(data['annualInvestment'])}
+        </li>
+      })
+    }
+  </ul>
+   }
   </div> 
   );
 }
---
+++ src/InputFeild.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export function InputFeild({udtInputData,symbol}){
+    const[val,newVal]=useState(0);
+    function udtInputVal(event){
+        const updatedValue = event.target.value;
+    newVal(updatedValue);
+        udtInputData(updatedValue,symbol);
+    }
+   
+    return(<ol id="user-input">
+        <label htmlFor={symbol}>{symbol}</label>
+        <li> <input type="number" value={val} onChange={udtInputVal} />
+       </li>
+       
+    </ol>)
+}
